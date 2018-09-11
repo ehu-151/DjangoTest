2dbeffc93822b5dcaa5cd41f2b705484eeffe7e2
ehu <fehuehuf@gmail.com>
2018-09-11 13:41:45 +0900

    urls.pyでurlを定義

--- manager_project/manager_project/urls.py
+++ manager_project/manager_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+import manager.views as manager_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^worker_list/', manager_view.WorkerListView.as_view())  # URLとViewを組み合わせる！
 ]
